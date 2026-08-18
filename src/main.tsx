54a364b5f8cf74759873b434016c108848dbb750
echcon <haunghia1512@gmail.com>
2026-08-18 10:40:25 +0700

    fix deploy alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import 'antd/dist/reset.css'
 import { Provider } from 'react-redux'
-import { store } from './app/redux/store.ts'
+import { store } from './app/redux/store'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
